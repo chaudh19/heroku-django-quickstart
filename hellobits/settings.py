58a62218f0bdef033ce15d5feb7bfcfd4e8b173e
simran <simran.d.chaudhry@gmail.com>
2020-06-13 09:59:13 -0700

    import dj_database_url

--- hellobits/settings.py
+++ hellobits/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -82,6 +83,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
